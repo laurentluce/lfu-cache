f487fe8bc7b5cd07a4c397c05bb7e329aa18e6f6
Laurent Luce <laurentluce49@yahoo.com>
2015-12-01 11:30:36 -0500

    Setup update for Pypi.
    Change-Id: I36ccfa93a3dc43daa53d57c8806f0f72d4d9d2fd

--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name='LFUCache',
-    version='0.1',
+    description='Cache with LFU eviction scheme.',
+    version='1.0.0',
     packages=['lfucache',],
     license='MIT',
+    author='Laurent Luce',
+    author_email='laurentluce49@yahoo.com',
+    url='https://github.com/laurentluce/lfu-cache',
+    keywords='lfu cache insertion access deletion',
     long_description='Cache with LFU eviction scheme implemented in Python with complexity O(1) for insertion, access and deletion.',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ),
 )
